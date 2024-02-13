c1134e525d869dc05acc2fa542c17ad57b5d043e
Eduardo Soares Azzolin <eduduazzolin@gmail.com>
2024-02-13 18:22:03 -0300

    markdown refactored

--- gerador_de_documentacao_pbi.py
+++ gerador_de_documentacao_pbi.py
@@ -26,7 +26,7 @@ class GeradorDeDocumentacaoPBI:
         md += f'# {self.model.name}\n'
         md += f'- Data do relatório: {dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}\n'
 
-        md += f'\n## Resumo do modelo de dados\n'
+        md += f'\n# Resumo do modelo de dados\n'
         md += f'- **Tamanho do modelo:** {((self.model.size / 1024) / 1024):.2f} MB\n'
         md += f'- **Quantidade de tabelas:** {len(self.model.tables)}\n'
         md += f'- **Quantidade de relacionamentos:** {len(self.model.relationships)}\n'
@@ -34,13 +34,13 @@ class GeradorDeDocumentacaoPBI:
         md += f'- **Quantidade de colunas calculadas:** {sum([len([c for c in table.table_itens if c.table_item_type == "calculated"]) for table in self.model.tables])}\n'
         md += f'- **Quantidade de medidas:** {sum([len([m for m in table.table_itens if m.table_item_type == "measure"]) for table in self.model.tables])}\n'
 
-        md += f'\n### Tabelas\n'
+        md += f'\n## Tabelas\n'
         count = 1
         for t in self.model.tables:
             md += f'{str(count)}. {t.name}\n'
             count += 1
 
-        md += f'\n### Relacionamentos\n\n'
+        md += f'\n## Relacionamentos\n\n'
         md += f'|  |  |  |  |  |\n'
         md += f'| ---- | ---- | ---- | ---- | ---- |\n'
         for r in self.model.relationships:
@@ -52,7 +52,7 @@ class GeradorDeDocumentacaoPBI:
             else:
                 md += f'| (Desativado) *{origin}* | *{r.origin_cardinality}* | {direction} | *{r.target_cardinality}* | *{target}*\n'
         md += f'|  |  |  |  |  |\n'
-        md += f'\n### Medidas\n'
+        md += f'\n## Medidas\n'
         count = 1
         for t in self.model.tables:
             for m in t.table_itens:
@@ -60,7 +60,7 @@ class GeradorDeDocumentacaoPBI:
                     md += f'{str(count)}. [{m.name}]\n'
                     count += 1
 
-        md += f'\n## Detalhamento das tabelas\n'
+        md += f'\n# Detalhamento das tabelas\n'
         for t in self.model.tables:
             is_in_relationship = False
             has_columns = False
@@ -80,14 +80,14 @@ class GeradorDeDocumentacaoPBI:
                 if i.table_item_type == 'calculated':
                     has_calculated_columns = True
 
-            md += f'\n### {t.name}\n'
+            md += f'\n## {t.name}\n'
             md += f'- **Nome:** {t.name}\n'
             md += f'- **Tipo:** {t.table_type}\n'
             md += f'- **Modo de importação:** {t.import_mode}\n'
             if t.description:
                 md += f'- **Descrição:** {" ".join(t.description)}\n'
 
-            md += f'\n#### Colunas\n' if has_columns else ''
+            md += f'\n### Colunas\n' if has_columns else ''
             count = 0
             for c in t.table_itens:
                 if c.table_item_type == 'column':
@@ -95,7 +95,7 @@ class GeradorDeDocumentacaoPBI:
                     md += f'{str(count)}. {c.name}\n'
 
             if is_in_relationship:
-                md += f'\n#### Relacionamentos\n'
+                md += f'\n### Relacionamentos\n'
                 md += f'|  |  |  |  |  |\n'
                 md += f'| ---- | ---- | ---- | ---- | ---- |\n'
                 when_both = [r for r in self.model.relationships if r.is_both_directions if
@@ -137,28 +137,28 @@ class GeradorDeDocumentacaoPBI:
                 md += f'|  |  |  |  |  |\n'
 
             if t.query:
-                md += f'\n#### Query:\n'
+                md += f'\n### Query:\n'
                 md += f'```sql\n'
                 md += f'{t.query}\n'
                 md += f'```\n'
 
             if t.power_query_steps:
-                md += f'\n#### Definição no PowerQuery:\n'
+                md += f'\n### Definição no PowerQuery:\n'
                 md += f'```M\n'
                 for step in t.power_query_steps:
                     md += f'{step}\n'
                 md += f'```\n'
 
-            md += f'\n#### Colunas calculadas\n' if has_calculated_columns else ''
+            md += f'\n### Colunas calculadas\n' if has_calculated_columns else ''
             for c in t.table_itens:
                 if c.table_item_type == 'calculated':
                     c_expression = '\n'.join(c.expression)
-                    md += f'\n##### {c.name}\n'
+                    md += f'\n**{c.name}**\n'
                     md += f'```dax\n'
                     md += f'{c_expression}\n'
                     md += f'```\n'
 
-            md += f'\n#### Medidas\n' if has_measures else ''
+            md += f'\n### Medidas\n' if has_measures else ''
             for m in t.table_itens:
                 if m.table_item_type == 'measure':
                     if m.expression:
@@ -168,7 +168,7 @@ class GeradorDeDocumentacaoPBI:
                         m_expression = '\n'.join(m.expression)
                     else:
                         m_expression = 'Vazio'
-                    md += f'\n##### {m.name}\n'
+                    md += f'\n**{m.name}**\n'
                     md += f'- **Nome:** {m.name}\n'
                     md += f'- **Pasta:** {m.display_folder if m.display_folder else "Nenhuma"}\n'
                     md += f'- **Formato:** ``{m.format_string if m.format_string else "Automático"}``\n'
